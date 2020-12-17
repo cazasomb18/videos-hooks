b94db4b8096ff67c0eb199a0b5237814009e1010
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-17 12:08:03 -0600

    finished app walk through, tested, app complete

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const KEY = 'AIzaSyDy79LL1Ec6TWub54MNR_m4XcIiD-v1i3M';
+
 
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
@@ -11,3 +13,4 @@ export default axios.create({
 
 	}
 });
+
